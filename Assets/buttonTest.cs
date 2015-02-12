44c05a7603b999c943efb2ba88916c8d8da4bd3c
roma_k <romankalinchuk@gmail.com>
2015-02-12 22:21:51 +0200

    next

---
+++ Assets/buttonTest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class buttonTest : MonoBehaviour {
+
+    public Slider slider;
+
+
+    public void DoSomething()
+    {
+        Debug.Log(slider.value.ToString());
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
