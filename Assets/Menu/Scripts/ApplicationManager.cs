f4094b27ac354b38c5a7d7bf005c65a38c5319f7
romak <romankalinchuk@gmail.com>
2015-02-23 17:11:58 +0200

    next

---
+++ Assets/Menu/Scripts/ApplicationManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ApplicationManager : MonoBehaviour
+{
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
+    }
+
+    public void SetMasterVolume(Slider slider)
+    {
+        audio.volume = slider.value;
+    }
+
+    public void SetVideoQuality(Slider slider)
+    {
+        QualitySettings.SetQualityLevel((int)slider.value, true);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        //audio.Play();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Menu/Scripts/PanelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PanelManager : MonoBehaviour {
+
+    public void OpenMenuPanel(GameObject panel) {
+        panel.SetActive(true);
+    }
+
+    public void CloseMenuPanel(GameObject panel){
+        panel.SetActive(false);
+    }
+
+}
