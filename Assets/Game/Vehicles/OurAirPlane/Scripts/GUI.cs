21ae3ecaf008ad7741ac7be4fbeb2226f69d4855
romak <romankalinchuk@gmail.com>
2015-03-02 17:14:44 +0200

    next

---
+++ Assets/Game/Vehicles/OurAirPlane/Scripts/GUI.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GUI : MonoBehaviour {
+
+    public Text guiText;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
