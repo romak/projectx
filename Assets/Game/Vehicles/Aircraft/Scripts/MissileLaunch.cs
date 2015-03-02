37a6740d8a91f1fadfe0c18869a1e91bf52f07d8
romak <romankalinchuk@gmail.com>
2015-03-02 16:58:28 +0200

    next

--- Assets/Game/Vehicles/Aircraft/Scripts/MissileLaunch.cs
+++ Assets/Game/Vehicles/Aircraft/Scripts/MissileLaunch.cs
@@ -4,24 +4,23 @@ using System.Collections;
 public class MissileLaunch : MonoBehaviour
 {
     public GameObject missile;
+    public Transform missilePos;
+    public float fireRate = 1.0f;
+    private float nextFire;
 
-    // Use this for initialization
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-	    	//Vector3 position = new Vector3(0, -0.2f, 1) * 0.5f;
-            Vector3 position = new Vector3(-10, -8, -1);
-            position = transform.TransformPoint(position);
-
-		    GameObject thisMissile  = (GameObject)Instantiate(missile, position, transform.rotation);
-		    Physics.IgnoreCollision(thisMissile.collider, collider);
+        //if ((Input.GetKeyDown(KeyCode.Space) || (Input.GetButton("Fire1"))) && (Time.time > nextFire))
+        if ((Input.GetKeyDown(KeyCode.Space)) && (Time.time > nextFire))
+            {
+            nextFire = Time.time + fireRate;
+            GameObject thisMissile = (GameObject)Instantiate(missile, missilePos.position, transform.rotation);
+            Physics.IgnoreCollision(thisMissile.collider, collider);
         }
 
     }
